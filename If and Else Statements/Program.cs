7f6dba6aae38af5b4c92445d087340b73338df20
Unauthorized23 <105158734+Unauthorized23@users.noreply.github.com>
2022-05-14 08:48:11 +0630

    Add project files.

---
+++ If and Else Statements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace TestStatements
+{
+    public class Statements
+    {
+        static void Main(string[] args)
+        {
+            string Car = "car";
+            string BMW = "car";
+
+            if (Car == BMW)
+            {
+                Console.WriteLine("BMW is equal as a Car!");
+            }
+            else
+            {
+                Console.WriteLine("BMW isn't equal as a Car!");
+            }
+        }
+    }
+}
